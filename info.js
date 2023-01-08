35674a366c5dd45ef01b7d74a87ad1ae9c639148
Niko Suoniemi <niko@tamperelainen.org>
2023-01-09 00:57:58 +0200

    Show back link before fetch

--- info.js
+++ info.js
@@ -3,15 +3,17 @@ import request from './request.js'
 
 export function Info() {
     ui.view(this, 'Info', true)
-    this.tree.innerHTML = '<p>Loading...</p>'
+    this.tree.innerHTML =
+        `<a target="List">&laquo; Back to list</a><br/>
+        <p>Loading...</p>`
     this.interval = 0
     this.compose = async () => {
         if (!this.data) return
         let json = await request.http(
             `https://swapi.dev/api/people/${this.data}`)
-        this.tree.innerHTML = `<a target="List">&laquo; Back to list</a><br/>`
-        for (const key in json)
-            this.tree.innerHTML += `<p>${key}: ${json[key]}</p>`
+        let content = ''
+        for (const key in json) content += `<p>${key}: ${json[key]}</p>`
+        this.tree.querySelector('p').innerHTML = content
         this.tree.querySelector('a')
             .addEventListener('click', this.setView)
     }
